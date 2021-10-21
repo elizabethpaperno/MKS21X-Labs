d97b77b4c68f291fc2ea77a0e9e61dd865673c1e
Elizabeth Paperno <liza.paperno@gmail.com>
2021-10-21 10:48:45 -0400

    Added 2 mutator methods: changeName(String) and makeOlder(), and toString.

--- 01-Object/Kitty.java
+++ 01-Object/Kitty.java
@@ -1,18 +1,27 @@
 public class Kitty {
   private int age;
   private String name;
-  public Kitty (int startAge, String startName){
+  public Kitty (String startName, int startAge){
       name = startName;
       age = startAge;
   }
   public Kitty(){
     name = "Mittens";
-    age = 2
+    age = 2;
   }
   public int getAge(){
     return age;
   }
   public String getName(){
     return name;
+  }
+  public void changeName(String newName){
+    name = newName;
+  }
+  public void makeOlder(){
+    age += 1;
+  }
+  public String toString(){
+    return "The amazing" + name;
   }
 }
