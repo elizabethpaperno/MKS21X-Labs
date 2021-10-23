9aa5efbd1f0c01e3d3c64a4b54e8984e87479a66
Elizabeth Paperno <liza.paperno@gmail.com>
2021-10-23 14:51:33 -0400

    Method distance added

--- 01-Object/Point.java
+++ 01-Object/Point.java
@@ -18,4 +18,8 @@ public class Point{
   public distanceTo(Point P2){
     return Math.sqrt(Math.pow(p.x - P2.x,2) + Math.pow(p.y - P2.y,2));
   }
+
+  public static distance(Point P1, Point P2){
+    return Math.sqrt(Math.pow(P1.x - P2.x,2) + Math.pow(P1.y - P2.y,2));
+  }
 }
