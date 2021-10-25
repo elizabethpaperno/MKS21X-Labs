47239035481b80af3b24898b08b8976214db9a73
Elizabeth Paperno <liza.paperno@gmail.com>
2021-10-25 10:46:44 -0400

    Added constructor w/ doubles

--- 02-Triangle/Triangle.java
+++ 02-Triangle/Triangle.java
@@ -5,4 +5,10 @@ public class Triangle {
     v2 = v_2;
     v3 = v_3;
   }
+
+  public Triangle(double v1x, double v1y, double v2x, double v2y, double v3x, double v3y){
+    v1 = Point(v1x, v1y);
+    v2 = Point(v2x, v2y);
+    v3 = Point(v3x, v3y);
+  }
 }
