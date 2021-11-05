b9576da96a8be2664754fbf769b9187f8129abed
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-05 10:58:11 -0400

    Phase 2 testing done

--- 04-SuperArray/Demo.java
+++ 04-SuperArray/Demo.java
@@ -7,5 +7,27 @@ public class Demo{
     System.out.println(super1.size()); //3
     System.out.println(super1); //[hi, hello, hey]
     System.out.println(super1.toStringDebug());//[hi, hello, hey, null, null, null, null, null, null, null]
+
+    //phase 2
+    //loop that prints the individual elements of the SuperArray using get.
+    for (int i=0; i < super1.size(); i++){ //hi \n hello \n hey \n
+      System.out.println(super1.get(i));
+    }
+    //loop that changes all of the elements to different values.
+    String[] newVals = {"bye", "goodbye", "adios"};
+    for (int i=0; i < super1.size(); i++){
+      super1.set(i, newVals[i]);
+    }
+    System.out.println(super1); //[bye,goodbye,adios]
+
+    //loop that sets each value equal to the next value, and replaces the last value with "NOPE!".
+    for (int i=0; i < super1.size(); i++){
+      if (i == super1.size() - 1){
+        super1.set(i, "NOPE!");
+      }else{
+        super1.set(i, super1.get(i+1));
+      }
+    }
+    System.out.println(super1); //goodbye, adios, NOPE!
   }
 }
