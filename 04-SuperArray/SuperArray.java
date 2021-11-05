bbeb386031e064e81eb61ee13f5429facda68de4
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-04 22:21:35 -0400

    fill in all basic methods

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -1,24 +1,42 @@
-public class SuperArray(){
+public class SuperArray{
   private int size;
   private int maxCap;
-  
+  private String[] supArr;
+
   public SuperArray(){
-    size = 0; 
-    maxCap = 10; 
-    String[] supArr = new String[maxCap];
+    size = 0;
+    maxCap = 10;
+    supArr = new String[maxCap];
   }
-  
-  public int size(String[] arr){
-    
+
+  public int size(){
+    return size;
   }
-  
-  public String[] add(String str){
-    
+
+  public void add(String str){
+    supArr[size] = str;
+    size += 1;
   }
-  
+
   public String toString(){
+    String arrStr = "[";
+    for (int i = 0; i < this.size(); i++){
+      arrStr += supArr[i];
+      if (i != this.size() - 1){
+        arrStr += ", ";
+      }
+    }
+    return arrStr + "]";
   }
-  
+
   public String toStringDebug(){
+    String arrStr = "[";
+    for (int i = 0; i < supArr.length; i++){
+      arrStr += supArr[i];
+      if (i != supArr.length - 1){
+        arrStr += ", ";
+      }
+    }
+    return arrStr + "]";
   }
 }
