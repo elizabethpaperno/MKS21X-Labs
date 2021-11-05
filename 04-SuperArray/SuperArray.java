ef9546bdbf68f1b2d640cbb549578d2d4942aba4
elizabethpaperno <67924232+elizabethpaperno@users.noreply.github.com>
2021-11-04 21:56:29 -0400

    Fill in no-param constructor, set up other methods

---
+++ 04-SuperArray/SuperArray.java
@@ -0,0 +1,24 @@
+public class SuperArray(){
+  private int size;
+  private int maxCap;
+  
+  public SuperArray(){
+    size = 0; 
+    maxCap = 10; 
+    String[] supArr = new String[maxCap];
+  }
+  
+  public int size(String[] arr){
+    
+  }
+  
+  public String[] add(String str){
+    
+  }
+  
+  public String toString(){
+  }
+  
+  public String toStringDebug(){
+  }
+}
