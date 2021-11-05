b534b46d7c798c76d1fb1a4fcfcf93ac4a8d2363
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-04 22:22:08 -0400

    Test cases added

--- 04-SuperArray/Demo.java
+++ 04-SuperArray/Demo.java
@@ -1,5 +1,11 @@
 public class Demo{
   public static void main(String[] args){
-    
+    SuperArray super1 = new SuperArray();
+    super1.add("hi");
+    super1.add("hello");
+    super1.add("hey");
+    System.out.println(super1.size()); //3
+    System.out.println(super1); //[hi, hello, hey]
+    System.out.println(super1.toStringDebug());//[hi, hello, hey, null, null, null, null, null, null, null]
   }
 }
