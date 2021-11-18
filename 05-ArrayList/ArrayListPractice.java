a52e8c9e8e9268a462070d5234846076c22c6bfa
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-17 20:37:14 -0500

    all testing done and methods working

--- 05-ArrayList/ArrayListPractice.java
+++ 05-ArrayList/ArrayListPractice.java
@@ -12,6 +12,7 @@ public class ArrayListPractice {
       }
     }
     replaceEmpty(a);
+
     System.out.println("Testing replaceEmpty");
     ArrayList<String> b = new ArrayList<String>();
     b.add("");
@@ -22,7 +23,63 @@ public class ArrayListPractice {
     b.add("");
     System.out.println(b);
     System.out.println(replaceEmpty(b));
-  }
+
+    System.out.println("Testing time on makeReversedList");
+    ArrayList<String> c = new ArrayList<String>(200000);
+    for (int i = 0; i < 200100;i++){
+      double rand = Math.random();
+      c.add("" + rand);
+    }
+    makeReversedList(c);
+
+    System.out.println("Testing makeReversedList");
+    ArrayList<String> d = new ArrayList<String>();
+    d.add("hi");
+    d.add("bye");
+    d.add("123");
+    d.add("hi");
+    System.out.println(d);
+    System.out.println(makeReversedList(d));
+
+    System.out.println("Testing time on mixLists");
+    ArrayList<String> e = new ArrayList<String>(200000);
+    for (int i = 0; i < 200100;i++){
+      double rand = Math.random();
+      e.add("" + rand);
+    }
+    ArrayList<String> h = new ArrayList<String>(200000);
+    for (int i = 0; i < 2001043;i++){
+      double rand = Math.random();
+      h.add("" + rand);
+    }
+    mixLists(e,h);
+
+    System.out.println("Testing mixLists");
+    ArrayList<String> f = new ArrayList<String>();
+    ArrayList<String> g = new ArrayList<String>();
+
+    f.add("1");
+    f.add("3");
+    f.add("5");
+    g.add("2");
+    g.add("4");
+    g.add("6");
+
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(mixLists(f,g));
+
+    f.add("7");
+    f.add("9");
+    System.out.println(mixLists(f,g));
+
+    g.add("8");
+    g.add("10");
+    g.add("12");
+    g.add("14");
+    System.out.println(mixLists(f,g));
+}
+
   public static ArrayList<String> replaceEmpty( ArrayList<String> data){
   //return an new ArrayList that has all of the values of the original ArrayList except
   //that all empty strings are replaced with the word "Empty".
@@ -40,7 +97,7 @@ public class ArrayListPractice {
   public static ArrayList<String> makeReversedList( ArrayList<String> data){
   //return a new ArrayList that is in the reversed order of the parameter.
     ArrayList<String> newArr = new ArrayList<String>();
-    for (int i = data.size(); i >= 0; i--){
+    for (int i = data.size()-1; i >= 0; i--){
       newArr.add(data.get(i));
     }
     return newArr;
@@ -64,13 +121,13 @@ public class ArrayListPractice {
       }
     }else {
       for (int i = 0; i < b.size(); i++){
-        newArr.add(b.get(i));
         if (i == a.size()-1){
           flag = true;
         }
         if (!flag){
           newArr.add(a.get(i));
         }
+        newArr.add(b.get(i));
       }
     }
     return newArr;
