dafdc8d91eedc47e48a3f573c056cbc35248c369
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-17 20:24:21 -0500

    testing for replaceEmpty done and passed

--- 05-ArrayList/ArrayListPractice.java
+++ 05-ArrayList/ArrayListPractice.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
 public class ArrayListPractice {
-  public static void main(String args){
-    ArrayList a = new ArrayList(200000);
+  public static void main(String[] args){
+    System.out.println("Testing time on replaceEmpty");
+    ArrayList<String> a = new ArrayList<String>(200000);
     for (int i = 0; i < 200100;i++){
       double rand = Math.random();
       if (rand < 0.1){
@@ -10,12 +11,22 @@ public class ArrayListPractice {
         a.add("" + rand);
       }
     }
-    a.replaceEmpty();
+    replaceEmpty(a);
+    System.out.println("Testing replaceEmpty");
+    ArrayList<String> b = new ArrayList<String>();
+    b.add("");
+    b.add("blalba");
+    b.add("hi");
+    b.add("");
+    b.add("beb");
+    b.add("");
+    System.out.println(b);
+    System.out.println(replaceEmpty(b));
   }
   public static ArrayList<String> replaceEmpty( ArrayList<String> data){
   //return an new ArrayList that has all of the values of the original ArrayList except
   //that all empty strings are replaced with the word "Empty".
-  ArrayList newArr = new ArrayList();
+  ArrayList<String> newArr = new ArrayList<String>();
     for (int i = 0; i < data.size(); i++){
       if(data.get(i).equals("")){
          newArr.add("Empty");
@@ -28,7 +39,7 @@ public class ArrayListPractice {
 
   public static ArrayList<String> makeReversedList( ArrayList<String> data){
   //return a new ArrayList that is in the reversed order of the parameter.
-    ArrayList newArr = new ArrayList();
+    ArrayList<String> newArr = new ArrayList<String>();
     for (int i = data.size(); i >= 0; i--){
       newArr.add(data.get(i));
     }
@@ -39,7 +50,7 @@ public class ArrayListPractice {
   //return a new ArrayList that has all values of a and b in alternating order that is:
   //a[0], b[0], a[1], b[1]...
   //when one list is longer than the other, just append the remaining values to the end.
-    ArrayList newArr = new ArrayList();
+    ArrayList<String> newArr = new ArrayList<String>();
     boolean flag = false;
     if (a.size() > b.size()){
       for (int i = 0; i < a.size(); i++){
@@ -61,7 +72,7 @@ public class ArrayListPractice {
           newArr.add(a.get(i));
         }
       }
-    return newArr;
     }
+    return newArr;
   }
 }
