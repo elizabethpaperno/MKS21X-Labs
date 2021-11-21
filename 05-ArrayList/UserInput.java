1098babc0be4f474be6586d5504b4c69c2ff9f2a
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-21 14:18:43 -0500

    fina output added

--- 05-ArrayList/UserInput.java
+++ 05-ArrayList/UserInput.java
@@ -9,8 +9,35 @@ public class UserInput{
       double num = in.nextDouble();
       if (num < 0){
         isNeg = true;
+      }else{
+        arrList.add(num);
       }
-      arrList.add(num);
     }
+    System.out.print("You entered: ");
+    System.out.println(arrList.toString());
+
+    double sum = 0;
+    double max = arrList.get(0);
+    double min = arrList.get(0);
+
+    for (int i=0; i < arrList.size(); i++){
+      sum += arrList.get(i);
+
+      if (arrList.get(i) > max){
+        max = arrList.get(i);
+      }
+
+      if (arrList.get(i) < min){
+        min = arrList.get(i);
+      }
+    }
+    System.out.print("Mean: ");
+    System.out.println(sum/arrList.size());
+
+    System.out.print("Min value: ");
+    System.out.println(min);
+
+    System.out.print("Max value: ");
+    System.out.println(max);
   }
 }
