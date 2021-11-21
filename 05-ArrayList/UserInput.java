7a7cd767449c0ddfc9976f828846ca65678b962a
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-21 14:00:57 -0500

    resolving changes made in class

--- 05-ArrayList/UserInput.java
+++ 05-ArrayList/UserInput.java
@@ -1,1 +1,11 @@
-
+import java.util.*;
+public class UserInput{
+  public static void main(String args[]){
+    double num = 0.0;
+    ArrayList<Double> arrList = new ArrayList<Double>();
+    while (!(num < 0.0)){
+      System.out.println("Enter a non negative number. A negative number will end the program:");
+      Scanner in = new Scanner(System.in);
+    }
+  }
+}
