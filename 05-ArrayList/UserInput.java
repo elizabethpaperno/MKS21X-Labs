60a63fac4adf4bd4f5cddee7b548a00fd4b3c647
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-21 14:04:03 -0500

    fix mistakes

--- 05-ArrayList/UserInput.java
+++ 05-ArrayList/UserInput.java
@@ -1,11 +1,16 @@
 import java.util.*;
 public class UserInput{
   public static void main(String args[]){
-    double num = 0.0;
+    boolean isNeg = false;
     ArrayList<Double> arrList = new ArrayList<Double>();
-    while (!(num < 0.0)){
+    while (!isNeg){
       System.out.println("Enter a non negative number. A negative number will end the program:");
       Scanner in = new Scanner(System.in);
+      double num = in.nextDouble();
+      if (num < 0){
+        isNeg = true;
+      }
+      arrList.add(num);
     }
   }
 }
