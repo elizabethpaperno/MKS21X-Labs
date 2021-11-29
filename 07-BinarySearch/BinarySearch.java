b1c64a2eb1abf43d6856c3f6c8514f91df4ba064
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-29 11:01:17 -0500

    class time work on conditionals

--- 07-BinarySearch/BinarySearch.java
+++ 07-BinarySearch/BinarySearch.java
@@ -2,9 +2,24 @@
  *@param data: the numbers to search through
  *@param target: the number you want to find
  *@return the index of data that contains the target value. Return -1 if the value is not in the array.
- *If there are multiple copies return the one you find first using 
+ *If there are multiple copies return the one you find first using
  *(min+max)/2 method described.
  */
-public static int binarySearch(int[]data,int target){
-  return 0;
+public class BinarySearch {
+  public static int binarySearch(int[]data,int target){
+    int start = 0;
+    int end = data.length() - 1;
+    int mid = end - start/2;
+    while(true){ //filler
+      mid = end - start/2;
+      if (target > mid){
+        start = mid + 1;
+      }
+      else if (target < mid){
+        end = mid - 1;
+      }else {
+        return mid;
+      }
+    }
+  }
 }
