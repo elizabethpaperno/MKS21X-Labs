27ff11f5befd81fed1873c6a169ab3b9f11aaa0c
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-30 10:37:37 -0500

    method setup

--- 06-Advent/Non.java
+++ 06-Advent/Non.java
@@ -1,2 +1,19 @@
 public class Non {
+  public static File readFile() {
+    try {
+        File file = new File(fileName);
+        return file;
+    }catch (FileNotFoundException e) {
+      System.out.println("File not found");
+      return null;
+    }
+  }
+  
+  public static void main(String[] args){
+   if (Integer.parseInt(args[1])==1){
+     getAmtWrapPaper(args[0]);
+   }else if (Integer.parseInt(args[1])==2){
+     getAmtRibbon(args[0]);
+   }
+ }
 }
