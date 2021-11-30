e53ebd85bbb1c02a577de10cb48b81d8122dc42f
Elizabeth Paperno <liza.paperno@gmail.com>
2021-11-30 10:42:25 -0500

    done with vowel part

--- 06-Advent/Non.java
+++ 06-Advent/Non.java
@@ -1,14 +1,21 @@
 public class Non {
-  public static File readFile() {
+  public static void part1() {
     try {
         File file = new File(fileName);
-        return file;
     }catch (FileNotFoundException e) {
       System.out.println("File not found");
-      return null;
     }
   }
-  
+
+  public static boolean hasVowel(String str){
+    int numV = 0;
+    for (int i = 0; i < str.length(), i++){
+      if (str.charAt(i) == 'a' || str.charAt(i) == 'i' || str.charAt(i) == 'e' || str.charAt(i) == 'u' || str.charAt(i) == 'o'){
+        numV += 1;
+      }
+    }
+    return (numV >= 3);
+  }
   public static void main(String[] args){
    if (Integer.parseInt(args[1])==1){
      getAmtWrapPaper(args[0]);
