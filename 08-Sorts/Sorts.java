b9809f357bbc067288853bd20c6d288816aedb34
Elizabeth Paperno <liza.paperno@gmail.com>
2021-12-01 10:39:46 -0500

    fixed print statement

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -26,9 +26,9 @@ public class Sorts{
   public static void main(String[] args){
     int[] test1 = {0,1,2,3,4,5,6};
     bubbleSort(test1);
-    System.out.println(test1.toString());
+    System.out.println(Arrays.toString(test1));
     int[] test2 = {1,0,4,7,6,2,3,5};
     bubbleSort(test2);
-    System.out.println(test2.toString());
+    System.out.println(Arrays.toString(test2));
   }
 }
