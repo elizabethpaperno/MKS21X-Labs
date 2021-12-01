e4f7f7e8d156e3cc3c8d3c1e5b6643a1efec87d7
Elizabeth Paperno <liza.paperno@gmail.com>
2021-12-01 10:38:16 -0500

    some of func done +test cases fixed

--- 08-Sorts/Sorts.java
+++ 08-Sorts/Sorts.java
@@ -6,13 +6,29 @@ public class Sorts{
   *@param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] data){
-
+    int end = data.length - 1;
+    int ogVal = 0;
+    boolean swapped = true;
+    while(swapped) {
+      swapped = false;
+      for (int i = 0; i < end; i++){
+        if (data[i] > data[i+1]){
+          swapped = true;
+          ogVal = data[i + 1];
+          data[i+1] = data[i];
+          data[i] = ogVal;
+        }
+      }
+      end -= 1;
+    }
   }
 
   public static void main(String[] args){
     int[] test1 = {0,1,2,3,4,5,6};
-    System.out.println(bubbleSort(test1).toString());
+    bubbleSort(test1);
+    System.out.println(test1.toString());
     int[] test2 = {1,0,4,7,6,2,3,5};
-    System.out.println(bubbleSort(test2).toString());
+    bubbleSort(test2);
+    System.out.println(test2.toString());
   }
 }
