b012df16e18165d92a2d38d92577f2d65ff13db9
Elizabeth Paperno <liza.paperno@gmail.com>
2021-12-10 10:56:19 -0500

    working omnidirectional, start work on new methods

--- 09-WordSearch/WordSearch.java
+++ 09-WordSearch/WordSearch.java
@@ -1,7 +1,22 @@
 /*Lab9: Word Search generator
 */
+
+import java.util.*; //random, scanner, arraylist
+import java.io.*; //file, filenotfoundexception
 public class WordSearch{
     private char[][]data;
+
+    //the random seed used to produce this WordSearch
+    private int seed;
+
+    //a random Object to unify your random calls
+    private Random randgen;
+
+    //all words that were successfully added get moved into wordsAdded.
+    private ArrayList<String>wordsAdded;
+
+    /*New Constructors:  Both will read in the word text file, then run addAllWords().
+     *Do not fill in random letters after.*/
 
     /**Initialize the grid to the size specified
      *and fill all of the positions with '_'
@@ -11,6 +26,22 @@ public class WordSearch{
     public WordSearch(int rows,int cols){
       data = new char[rows][cols];
       this.clear();
+    }
+
+    /*New Constructors:  Both will read in the word text file, then run addAllWords().
+     *Do not fill in random letters after.*/
+    public WordSearch( int rows, int cols, String fileName){
+        //Choose a randSeed using the clock random
+        data = new char[rows][cols];
+        randgen = new Random();
+        seed = randgen.nextInt();
+        addAllWords();
+    }
+
+    public WordSearch( int rows, int cols, String fileName, int randSeed){
+        //Use the random seed specified.
+        data = new char[rows][cols];
+        addAllWords();
     }
 
     /**Set all values in the WordSearch to underscores'_'*/
@@ -49,6 +80,7 @@ public class WordSearch{
      * or there are overlapping letters that do not match, then false is returned
      * and the board is NOT modified.
      */
+     /*
     public boolean addWordHorizontal(String word,int row, int col){
       int strIndex = 0;
       if (data[row].length - col >= word.length()){
@@ -66,6 +98,7 @@ public class WordSearch{
       }
       return false;
     }
+    */
 
 
    /**Attempts to add a given word to the specified position of the WordGrid.
@@ -79,6 +112,7 @@ public class WordSearch{
      *or there are overlapping letters that do not match, then false is returned.
      *and the board is NOT modified.
      */
+    /*
     public boolean addWordVertical(String word,int row, int col){
       int strIndex = 0;
       if (data.length - row >= word.length()){
@@ -95,6 +129,7 @@ public class WordSearch{
       }
       return false;
     }
+    */
 
     /**Attempts to add a given word to the specified position of the WordGrid.
      *The word is added from top left towards the bottom right, it must fit on the board,
@@ -106,6 +141,7 @@ public class WordSearch{
      *@return true when the word is added successfully. When the word doesn't fit,
      *or there are overlapping letters that do not match, then false is returned.
      */
+     /*
     public boolean addWordDiagonal(String word,int row, int col){
       int strIndex = 0;
       if (data.length - row >= word.length()){
@@ -126,31 +162,64 @@ public class WordSearch{
       }
       return false;
     }
+    */
 
+    /**Attempts to add a given word to the specified position of the WordGrid.
+    *The word is added in the direction rowIncrement,colIncrement
+    *Words must have a corresponding letter to match any letters that it overlaps.
+    *
+    *@param word is any text to be added to the word grid.
+    *@param row is the vertical locaiton of where you want the word to start.
+    *@param col is the horizontal location of where you want the word to start.
+    *@param rowInc is -1,0, or 1 and represents the displacement of each letter in the row direction
+    *@param colInc is -1,0, or 1 and represents the displacement of each letter in the col direction
+    *@return true when: the word is added successfully.
+    *        false (and do not change the board at all) when any of the following happen:
+    *        a) rowInc and colInc are both 0,
+    *        b) the word doesn't fit,
+    *        c) there are overlapping letters that do not match
+    */
     public boolean addWord(int row, int col, String word, int rowInc, int colInc){
-      if ((rowInc == 0 && colInc == 0)|| rowInc < -1 || rowInc > 1 || colInc < -1 || colInc > 1){
+      if (rowInc == 0 && colInc == 0){
         // if input is invalid
         //System.out.println("invalid increment");
         return false;
       }
-      int xFit = row + (word.length() - 1) * colInc;
-      int yFit = col + (word.length() - 1) * rowInc;
+
+      //check if word fits
+      int xFit = row + (word.length() - 1) * rowInc;
+      int yFit = col + (word.length() - 1) * colInc;
       if (!((xFit <= data[row].length && xFit >= 0) && (yFit <= data.length && yFit >= 0))){
-        System.out.println("doesn't fit");
+        //System.out.println("doesn't fit");
         return false;
       }
+
+      //checks for incorrect overlap
+      int iTest = row;
+      int jTest = col;
+      for(int x = 0; x < word.length(); x++){
+        if (!(data[iTest][jTest]== '_' || data[iTest][jTest] == word.charAt(x))){
+          return false;
+        }
+      }
+
       int i = row;
       int j = col;
       for(int x = 0; x < word.length(); x++){
-        if (data[i][j]== '_' || data[i][j] == word.charAt(x)){
-          //System.out.println(j + ", " + col);
-          data[i][j] = word.charAt(x);
-        } else {
-          return false;
-        }
-        i += colInc;
-        j += rowInc;
+        //System.out.println(j + ", " + col);
+        data[i][j] = word.charAt(x);
+        i += rowInc;
+        j += colInc;
       }
       return true;
+    }
+
+    private boolean addWord(String word,int row, int col, int rowInc, int colInc){
+      return false;
+    }
+
+
+    private void addAllWords() {
+      /*Attempt to add all of the words from the wordsToAdd list using the algorithm described above*/
     }
 }
