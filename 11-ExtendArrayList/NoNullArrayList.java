e13ae244546f7f44f019211a97229441c04616b5
Elizabeth Paperno <liza.paperno@gmail.com>
2021-12-16 10:48:24 -0500

    throw exception in methods

--- 11-ExtendArrayList/NoNullArrayList.java
+++ 11-ExtendArrayList/NoNullArrayList.java
@@ -1,13 +1,19 @@
 public class NoNullArrayList<T> extends ArrayList<T> {
   private T value;
   public boolean add(T element){
-
+    if (element == null){
+      throw new IllegalArgumentException("Cannot add null object");
+    }
   }
   public void add(int index, T element){
-
+    if (element == null){
+      throw new IllegalArgumentException("Cannot add null object");
+    }
   }
 
   public E set(int index, T element){
-    
+    if (element == null){
+      throw new IllegalArgumentException("Cannot add null object");
+    }
   }
 }
