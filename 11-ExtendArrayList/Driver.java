ac323e0b8bfa1af5dfbe0f47362dcdfa68375af0
Elizabeth Paperno <liza.paperno@gmail.com>
2021-12-16 10:59:10 -0500

    Test cases made

---
+++ 11-ExtendArrayList/Driver.java
@@ -0,0 +1,30 @@
+public class Driver{
+  public static void main(String[] args){
+    NoNullArrayList arr1 = new NoNullArrayList();
+    try {
+      arr1.add(3);
+      arr1.add(5);
+      arr1.add(null); //throws exception
+    }catch(IllegalArgumentException e){
+      System.out.println("add null to end caught");
+    }
+
+    NoNullArrayList arr2 = new NoNullArrayList();
+    try {
+      arr2.add(3);
+      arr2.set(0, null); //throws exception
+    }catch(IllegalArgumentException e){
+      System.out.println("set to null caught");
+    }
+
+    NoNullArrayList arr3 = new NoNullArrayList();
+    try {
+      arr3.add(0, 3);
+      arr3.add(1, 5);
+      arr3.add(2, null); //throws exception
+    }catch(IllegalArgumentException e){
+      System.out.println("add null to position caught");
+    }
+
+  }
+}
