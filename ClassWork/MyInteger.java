31bb3ced9beadc1c1b90c1c468f464e2290b01aa
Elizabeth Paperno <liza.paperno@gmail.com>
2021-12-21 10:53:45 -0500

    first try

--- ClassWork/MyInteger.java
+++ ClassWork/MyInteger.java
@@ -1,8 +1,16 @@
-public class MYInteger {
+public class MyInteger {
   public static int stringToInt(String s){
       //return the value of Integer.parseInt(s) but do it yourself!
       //Do not use any built in parse methods.
+      int finalNum = 0;
+      int digit = s.length()-1 * 10;
+      for (int i = 0; i < s.length(); i++){
+        finalNum += valueOfDigit(s.charAt(i)) * digit;
+        digit /= 10;
+      }
+      return finalNum;
   }
   public static int valueOfDigit(char c){
+    return (int(c))
   }
 }
