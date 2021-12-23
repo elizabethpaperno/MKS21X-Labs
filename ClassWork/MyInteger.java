cecf9de6baae428ee060ab938f314ebf948bf62f
Elizabeth Paperno <liza.paperno@gmail.com>
2021-12-22 19:27:57 -0500

    all working and neg case added: exit

--- ClassWork/MyInteger.java
+++ ClassWork/MyInteger.java
@@ -3,19 +3,25 @@ public class MyInteger {
       //return the value of Integer.parseInt(s) but do it yourself!
       //Do not use any built in parse methods.
       int finalNum = 0;
-      int digit = s.length()-1 * 10;
-      for (int i = 0; i < s.length(); i++){
+      int digit = (int)Math.pow(10,s.length()-1);
+      int startVal = 0;
+      int multiplier = 1;
+      if (s.charAt(0) == '-'){
+        startVal = 1;
+        multiplier = -1;
+        digit = (int)Math.pow(10,s.length()-2);
+      }
+      for (int i = startVal; i < s.length(); i++){
         finalNum += valueOfDigit(s.charAt(i)) * digit;
         digit /= 10;
-        System.out.println(finalNum);
       }
-      return finalNum;
+      return finalNum * multiplier;
   }
   public static int valueOfDigit(char c){
     return ((int) (c - '0'));
   }
   public static void main(String[] args){
     System.out.println(stringToInt("123"));
-
+    System.out.println(stringToInt("-123"));
   }
 }
