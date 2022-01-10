8c7ee0d77ffab14aed961f61719808c601196260
Elizabeth Paperno <liza.paperno@gmail.com>
2022-01-09 20:47:01 -0500

    tests working

--- 12-Stuyablo/Game2.java
+++ 12-Stuyablo/Game2.java
@@ -1,18 +1,24 @@
 public class Game2{
   public static void main(String[] args) {
     Adventurer a = new Wizard();
-    Adventurer b = new Wizard("Henry");
+    Adventurer b = new Wizard("Harry Potter",5);
     System.out.println(a+" ("+a.getHP()+"HP)");
     System.out.println(b+" ("+b.getHP()+"HP)");
     a.attack(b);
     b.attack(a);
     System.out.println(a+" ("+a.getHP()+"HP)");
     System.out.println(b+" ("+b.getHP()+"HP)");
-    /*
-    a.specialAttack(b);
+
+    b.attack(a);
+    b.attack(a);
+    b.attack(a);
+    b.attack(a);
+
+    a.specialAttack(b); //won't work
     b.specialAttack(a);
     System.out.println(a+" ("+a.getHP()+"HP)");
     System.out.println(b+" ("+b.getHP()+"HP)");
-    */
+
+    b.attack(a);
     }
 }
