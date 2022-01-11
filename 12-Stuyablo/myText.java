10c09d54122f05fa3e124ce895dd30d3e24f623b
Elizabeth Paperno <liza.paperno@gmail.com>
2022-01-11 11:00:12 -0500

    class time

--- 12-Stuyablo/myText.java
+++ 12-Stuyablo/myText.java
@@ -6,4 +6,21 @@ public class myText{
     }
     return myStr;
   }
+  public static void main(String[] args){
+    Text.hideCursor();
+    Text.clear();
+    Text.go(1,1);
+    System.out.print(Text.colorize(createEmpty(80), Text.BLUE+Text.BACKGROUND));
+    /*
+    for (int i = 2; i <= 29; i++){
+      Text.go(1,i);
+      System.out.print(Text.colorize(createEmpty(1), Text.BLUE+Text.BACKGROUND));
+      Text.go(81,i);
+      System.out.print(Text.colorize(createEmpty(1), Text.BLUE+Text.BACKGROUND));
+    }
+    */
+    Text.go(1,30);
+    System.out.print(Text.colorize(createEmpty(80), Text.BLUE+Text.BACKGROUND));
+    Text.go(1,31);
+  }
 }
