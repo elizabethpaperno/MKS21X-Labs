1aae714c35543e4bd305d44878891a87cfa04c7c
Elizabeth Paperno <liza.paperno@gmail.com>
2022-01-14 10:32:54 -0500

    changes to adventurer

--- 12-Stuyablo/Adventurer.java
+++ 12-Stuyablo/Adventurer.java
@@ -2,6 +2,7 @@ import java.util.Random;
 public abstract class Adventurer implements Damageable{
     private String name;
     private int HP;
+    private int maxHP;
 
     //Abstract methods are meant to be implemented in child classes.
     public abstract void attack(Damageable other);
@@ -46,6 +47,23 @@ public abstract class Adventurer implements Damageable{
     public void setName(String s){
 	     this.name = s;
     }
+    public int getmaxHP(){
+            return maxHP;
+          }
+          public void setmaxHP(int newMax){
+            maxHP = newMax;
+          }
 
+    //return a String that contains the description of the results of the attack
+    public abstract String attack(Damageable other);
 
+    //return a String that contains the description of the results of the specialAttack
+    public abstract String specialAttack(Damageable other);
+
+    //return the name of the special resource the class uses
+    public abstract String getSpecialName();
+
+    //return the value of the current and maximum special resource
+    public abstract int getSpecial();
+    public abstract int getSpecialMax();
 }
